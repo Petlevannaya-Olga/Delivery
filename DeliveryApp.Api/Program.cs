3edffb7f91bb68c2b01dfa3cd0f7e98e246ba825
Petlevannaya-Olga <olga.g.afonina@gmail.com>
2025-11-28 15:58:14 +0300

    DI

--- DeliveryApp.Api/Program.cs
+++ DeliveryApp.Api/Program.cs
@@ -1,7 +1,16 @@
+using System.Reflection;
+using CSharpFunctionalExtensions;
 using DeliveryApp.Api;
+using DeliveryApp.Core.Application.UseCases.Commands.AssignOrder;
+using DeliveryApp.Core.Application.UseCases.Commands.CreateOrder;
+using DeliveryApp.Core.Application.UseCases.Commands.MoveCouriers;
+using DeliveryApp.Core.Application.UseCases.Queries.GetAllCouriers;
+using DeliveryApp.Core.Application.UseCases.Queries.GetUncompletedOrders;
 using DeliveryApp.Core.Domain.Services;
+using DeliveryApp.Core.Ports;
 using DeliveryApp.Infrastructure.Adapters.Postgres;
-
+using DeliveryApp.Infrastructure.Adapters.Postgres.Repositories;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Primitives;
 
@@ -13,11 +22,11 @@ builder.Services.AddHealthChecks();
 // Cors
 builder.Services.AddCors(options =>
 {
-	options.AddDefaultPolicy(
-		policy =>
-		{
-			policy.AllowAnyOrigin(); // Не делайте так в проде!
-		});
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.AllowAnyOrigin(); // Не делайте так в проде!
+        });
 });
 
 // Configuration
@@ -29,28 +38,43 @@ builder.Services.AddScoped<IDispatchService, DispatchService>();
 
 // БД, ORM 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-{
-	options.UseNpgsql(connectionString,
-		sqlOptions => { sqlOptions.MigrationsAssembly("DeliveryApp.Infrastructure"); });
-	options.EnableSensitiveDataLogging();
-}
+    {
+        options.UseNpgsql(connectionString,
+            sqlOptions => { sqlOptions.MigrationsAssembly("DeliveryApp.Infrastructure"); });
+        options.EnableSensitiveDataLogging();
+    }
 );
 
 // UnitOfWork
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 // Repositories
-//builder.Services.AddScoped<ICourierRepository, CourierRepository>();
-//builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<ICourierRepository, CourierRepository>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+
+// MediatR
+builder.Services.AddMediatR(config => config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+// Commands
+builder.Services.AddTransient<IRequestHandler<CreateOrderCommand, UnitResult<Error>>, CreateOrderCommandHandler>();
+builder.Services.AddTransient<IRequestHandler<MoveCouriersCommand, UnitResult<Error>>, MoveCouriersCommandHandler>();
+builder.Services.AddTransient<IRequestHandler<AssignOrderCommand, UnitResult<Error>>, AssignOrderCommandHandler>();
+
+// Queries
+builder.Services.AddTransient<IRequestHandler<GetAllCouriersQuery, GetAllCouriersResponse>>(_ =>
+    new GetAllCouriersQueryHandler(connectionString));
+
+builder.Services.AddTransient<IRequestHandler<GetUncompletedOrdersQuery, GetUncompletedOrdersResponse>>(_ =>
+    new GetUncompletedOrdersQueryHandler(connectionString));
 
 var app = builder.Build();
 
 // -----------------------------------
 // Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
-	app.UseDeveloperExceptionPage();
+    app.UseDeveloperExceptionPage();
 else
-	app.UseHsts();
+    app.UseHsts();
 
 app.UseHealthChecks("/health");
 app.UseRouting();
@@ -58,8 +82,8 @@ app.UseRouting();
 // Apply Migrations
 using (var scope = app.Services.CreateScope())
 {
-	var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-	db.Database.Migrate();
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
 }
 
 app.Run();
