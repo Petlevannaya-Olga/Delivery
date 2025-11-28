f9c3324272089d46fa8361ca0899cd5ddab1c85c
Petlevannaya-Olga <olga.g.afonina@gmail.com>
2025-11-28 14:09:27 +0300

    CreateOrderCommand

---
+++ DeliveryApp.Core/Application/Commands/CreateOrderCommand.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using CSharpFunctionalExtensions;
+using MediatR;
+using Primitives;
+
+namespace DeliveryApp.Core.Application.Commands;
+
+public class CreateOrderCommand : IRequest<UnitResult<Error>>
+{
+    /// <summary>
+    /// Идентификатор заказа
+    /// </summary>
+    public Guid OrderId { get; }
+
+    /// <summary>
+    /// Улица
+    /// </summary>
+    /// <remarks>Корзина содержала полный Address, но для упрощения мы будем использовать только Street из Address</remarks>
+    public string Street { get; }
+
+    /// <summary>
+    /// Объем
+    /// </summary>
+    public int Volume { get; }
+
+    /// <summary>
+    /// Конструктор
+    /// </summary>
+    public CreateOrderCommand(Guid orderId, string street, int volume)
+    {
+        OrderId = orderId;
+        Street = street;
+        Volume = volume;
+    }
+    
+    /// <summary>
+    /// Fabric Method
+    /// </summary>
+    /// <param name="orderId">ID заказа</param>
+    /// <param name="street"> Аддрес</param>
+    /// <param name="volume">Размер заказа</param>
+    /// <returns></returns>
+    public static Result<CreateOrderCommand, Error> Create(Guid orderId, string street, int volume)
+    {
+        if (orderId == Guid.Empty) 
+            return GeneralErrors.ValueIsRequired(nameof(OrderId));
+        
+        if (string.IsNullOrEmpty(street)) 
+            return GeneralErrors.ValueIsRequired(nameof(Street));
+        
+        if (volume <= 0) 
+            return Errors.CantAssignCreateOrderWithWrongVolume();
+
+        return new CreateOrderCommand(orderId, street, volume);
+    }
+    
+    /// <summary>
+    /// Ошибки, которые может возвращать сущность
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public static class Errors
+    {
+        public static Error CantAssignCreateOrderWithWrongVolume()
+        {
+            return new Error($"{nameof(Volume).ToLowerInvariant()}.cant.create.order.with.wrong.volume",
+                $"Нельзя создать заказ, у которого вес <= 0");
+        }
+    }
+}
---
+++ DeliveryApp.Core/Application/Commands/CreateOrderCommandHandler.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using CSharpFunctionalExtensions;
+using DeliveryApp.Core.Domain.Model.OrderAggregate;
+using DeliveryApp.Core.Ports;
+using MediatR;
+using Primitives;
+
+namespace DeliveryApp.Core.Application.Commands;
+
+public class CreateOrderCommandHandler : IRequestHandler<CreateOrderCommand, UnitResult<Error>>
+{
+    private readonly IOrderRepository _orderRepository;
+    private readonly IGeoClient _geoClient;
+    private readonly IUnitOfWork _unitOfWork;
+
+    /// <summary>
+    ///     Ctr
+    /// </summary>
+    public CreateOrderCommandHandler(IUnitOfWork unitOfWork, IOrderRepository orderRepository, IGeoClient geoClient)
+    {
+        _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+        _orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
+        _geoClient = geoClient;
+    }
+
+    public async Task<UnitResult<Error>> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+    {
+        // Если заказ существует - ничего не делаем
+        var order = await _orderRepository.GetAsync(request.OrderId);
+        if (order.HasValue)
+            return UnitResult.Success<Error>();
+
+        // Получаем координаты
+        var location = await _geoClient.GetLocation(request.Street, cancellationToken);
+        if (location.IsFailure)
+            return Errors.LocationIsInvalid();
+
+        // Создаем заказ
+        var createOrderResult = Order.Create(request.OrderId, location.Value);
+        if (createOrderResult.IsFailure)
+            return createOrderResult.Error;
+
+        // Сохраняем заказ
+        await _orderRepository.Add(createOrderResult.Value);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+        return UnitResult.Success<Error>();
+    }
+
+    /// <summary>
+    /// Ошибки, которые может возвращать сущность
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public static class Errors
+    {
+        public static Error LocationIsInvalid()
+        {
+            return new Error($"location.is.invalid",
+                $"Ошибка при получении координат заказа");
+        }
+    }
+}
