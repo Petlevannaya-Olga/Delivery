7be1f56c50da3e4b5123866b5307de014c90f421
Petlevannaya-Olga <olga.g.afonina@gmail.com>
2025-11-28 15:45:44 +0300

    GetUncompletedOrders

---
+++ DeliveryApp.Core/Application/UseCases/CommonDtos/LocationDto.cs
@@ -0,0 +1,14 @@
+﻿namespace DeliveryApp.Core.Application.UseCases.CommonDtos;
+
+public class LocationDto
+{
+    /// <summary>
+    /// Координата по оси X
+    /// </summary>
+    public int X { get; set; }
+
+    /// <summary>
+    /// Координата по оси Y
+    /// </summary>
+    public int Y { get; set; }
+}
---
+++ DeliveryApp.Core/Application/UseCases/Queries/GetUncompletedOrders/GetUncompletedOrdersQueryHandler.cs
@@ -0,0 +1,43 @@
+﻿using Dapper;
+using DeliveryApp.Core.Application.UseCases.CommonDtos;
+using DeliveryApp.Core.Application.UseCases.Queries.GetAllCouriers;
+using DeliveryApp.Core.Domain.Model.OrderAggregate;
+using MediatR;
+using Npgsql;
+
+namespace DeliveryApp.Core.Application.UseCases.Queries.GetUncompletedOrders;
+
+public class GetUncompletedOrdersQueryHandler: IRequestHandler<GetUncompletedOrdersQuery, GetUncompletedOrdersResponse>
+{
+    private readonly string _connectionString;
+    public GetUncompletedOrdersQueryHandler(string connectionString)
+    {
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            throw new ArgumentNullException(nameof(connectionString));
+        }
+        _connectionString = connectionString;
+    }
+    
+    public async Task<GetUncompletedOrdersResponse> Handle(GetUncompletedOrdersQuery request, CancellationToken cancellationToken)
+    {
+        await using var connection = new NpgsqlConnection(_connectionString);
+        await connection.OpenAsync(cancellationToken);
+
+        var query = @"Select id, location_x, location_y from public.orders where order_status <> 'completed'";
+
+        var result = await connection.QueryAsync<OrderDto, LocationDto, OrderDto>
+        (
+            query,
+            (order, location) =>
+            {
+                order.LocationDto = location;
+                return order;
+            },
+            splitOn: "X",
+            param: new { statuses = new[] { OrderStatus.Created.Name, OrderStatus.Assigned.Name } }
+        );
+        
+        return new GetUncompletedOrdersResponse(result.ToList());
+    }
+}
